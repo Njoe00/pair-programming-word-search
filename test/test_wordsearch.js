abd8e59357edda168d2c0a913f91e6169c06542e
Njoe00 <nick.james.joe@gmail.com>
2022-10-26 18:07:05 -0700

    using loops to transpose a word search

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,58 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+
+  it("return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+  
+  it('returns false when given an empty array', function() {
+    const result = wordSearch([[]]);
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if word is a number", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 4444);
+
+    assert.isFalse(result);
+  })
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    if (letters.length > 0) {
+      const horizontalJoin = letters.map(ls => ls.join(''));
+      for (l of horizontalJoin) {
+        if (l.includes(word)) return true;
+      }
+      const verticalJoin = transpose(letters).map(ls => ls.join(''));
+      for (l of verticalJoin) {
+        if (l.includes(word)) return true;
+      }
     }
-}
+      return false;
+      
+  };  
+
+  const transpose = (letters) => {
+    
+    let vertical = [];
+    
+    for (let i = 0; i < letters[0].length; i++){
+      vertical.push([])
+      for (let j = 0; j < letters.length; j++) {
+        vertical[i][j] = letters[j][i];
+      }
+    }
+    return vertical;
+  }
+
+
 
 module.exports = wordSearch
+
